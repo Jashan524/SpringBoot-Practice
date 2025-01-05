28bbe9e4cd594e865ae5ce485f2dc701b68bf1d8
Jashanpreet Singh <jashanpreets524@gmail.com>
2025-01-05 16:36:55 +0530

    first Commit

---
+++ ecom-project/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Product (name, description, brand, price, category, release_Date, product_Available, stock_Quantity) VALUES
+('Smartphone', 'Latest 5G smartphone', 'TechBrand', 799.99, 'Electronics', '2024-01-15', true, 50);
